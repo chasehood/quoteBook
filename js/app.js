eeae7c79cb7cba271f67110f219a0233b74e1584
CHASE <t.chasepettyjohn@yahoo.com>
2017-05-16 20:00:58 -0600

    complete project

--- js/app.js
+++ js/app.js
@@ -1,1 +1,1 @@
-angular.module("quoteBook",[]);
+angular.module("quotebook",[]);
--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,4 +1,17 @@
-angular.module("quoteBook").controller("mainCtrl", function($scope, dataService){
-
-$scope.quotes = dataService.qetQuotes();
-});
+angular.module('quotebook').controller('mainCtrl', function ($scope, dataService) {
+    $scope.test = "this is working";
+    $scope.quotes = dataService.getQuotes();
+    $scope.deleteMe = function (textToDelete) {
+        dataService.removeData(textToDelete);
+    }
+    $scope.addQuote = function () {
+        var newQuote = {
+            text: $scope.newQuoteText,
+            author: $scope.newQuoteAuthor
+        }
+        if (dataService.addData(newQuote)) {
+            $scope.newQuoteText = '';
+            $scope.newQuoteAuthor = '';
+        }
+    }
+})
